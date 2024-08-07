5f7e6b762542411a315f4c227576470fe3ad69e1
Lorik Rama <lorik.rama.317@gmail.com>
2024-08-07 09:30:14 +0200

    added new experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,6 +8,21 @@ const Experience = () => {
         <div className="experience-container">
           <div className="info-container">
             <div className="info-title">
+              <h3>System Administrator</h3>
+              <p>June 2024 - Present</p>
+            </div>
+            <div className="info-company">
+              <h3>Unimax</h3>{" "}
+            </div>
+            <div className="info-desc">
+              <p>
+                A system administrator at Unimax is responsible for managing,
+                maintaining, and supporting the company's IT infrastructure.
+              </p>
+            </div>
+          </div>
+          <div className="info-container">
+            <div className="info-title">
               <h3>Software Engineer Instructor</h3>
               <p>Sept 2023 - Present </p>
             </div>
